96aa64d0014bcfcf3d3ff6ca1c428ce9a047f5a6
ErraticCreation <ErraticCreation@gmail.com>
2016-10-24 22:13:06 -0400

    Updated copy and styles on the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,28 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This weather application built on React was created as a means to familiarize myself with some of the various features within the React Framework.
+      </p>
+      <h4 className="text-center">
+        Some of the tools used:
+      </h4>
+      <dl>
+        <dt>
+          <a href="https://facebook.github.io/react/">React</a></dt>
+        <dd>
+          This was the JavaScript Framework used in the creation of this app.
+        </dd>
+      </dl>
+      <dl>
+        <dt>
+          <a href="https://openweathermap.org/">OpenWeatherMap</a>
+        </dt>
+        <dd>
+          Open Weather Map API was used in order to fetch current Weather Data.
+        </dd>
+      </dl>
     </div>
   );
 };
